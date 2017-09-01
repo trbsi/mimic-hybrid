27608e6ea397063a477414b516e29a870163ec2c
Dario Trbovic <timmy.dario@gmail.com>
2017-09-01 22:39:50 +0200

    working on video player

--- src/pages/listing/listing.ts
+++ src/pages/listing/listing.ts
@@ -8,6 +8,8 @@ import { ListingModel } from './listing.model';
 import { ListingService } from './listing.service';
 import { ProfilePage } from '../profile/profile';
 import { FabContainer } from 'ionic-angular';
+import { VgAPI } from 'videogular2/core';
+import { VideoPlaylistModel } from '../video-playlist/video-playlist.model';
 
 @Component({
   selector: 'listing-page',
@@ -17,6 +19,10 @@ export class ListingPage {
   listing: ListingModel = new ListingModel();
   loading: any;
   mainMenuOpened: boolean;
+
+  start_playing: boolean = false;
+  api: VgAPI;
+  video_playlist_model: VideoPlaylistModel = new VideoPlaylistModel();
 
   constructor(
     public nav: NavController,
@@ -83,4 +89,55 @@ export class ListingPage {
     {   
         alert(id);
     }
+
+
+    createLoader(){
+        this.loading = this.loadingCtrl.create();
+    }
+
+    presentLoader(){
+        // Check if the current instance is usable
+        if (this.loading === undefined || (this.loading !== undefined && this.loading.instance === null)){
+            // If it's not usable, then create a new one
+            this.createLoader();
+        }
+
+        this.loading.present();
+    }
+
+    dismissLoader(){
+        // Check if the current instance is usable
+        if (this.loading !== undefined){
+            // If it's not usable, then create a new one
+            this.loading.dismiss();
+        }
+    }
+
+    playMedia(media) {
+    // Check if this media is not the same we are currently playing
+    if(media !== this.video_playlist_model.selected_video)
+    {
+      this.presentLoader();
+          // Change sources
+          this.video_playlist_model.selected_video = media;
+          // When changing sources we wait until the metadata is loaded and then we start playing the video
+    }
+    }
+
+    onPlayerReady(api: VgAPI) {
+    this.api = api;
+        this.api.getDefaultMedia().subscriptions.loadedMetadata.subscribe(this.playVideo.bind(this));
+    }
+
+  playVideo() {
+        if(this.start_playing)
+        {
+            this.dismissLoader();
+        this.api.play();
+        }
+        else
+        {
+            this.start_playing = true;
+        }
+  }
 }

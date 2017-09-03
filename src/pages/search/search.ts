a975e66b46ee34d884027933d02db5f607f67c3b
Dario Trbovic <timmy.dario@gmail.com>
2017-09-03 21:38:21 +0200

    added search bar

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -5,6 +5,8 @@ import 'rxjs/Rx';
 
 import { SearchModel } from './search.model';
 import { SearchService } from './search.service';
+import { ViewChild } from '@angular/core';
+import { Searchbar } from 'ionic-angular';
 
 @Component({
   selector: 'searcg-page',
@@ -14,22 +16,33 @@ export class Search {
   list2: SearchModel = new SearchModel();
   loading: any;
 
+  @ViewChild(Searchbar) searchbar: Searchbar;
+
   constructor(
     public nav: NavController,
     public list2Service: SearchService,
     public loadingCtrl: LoadingController
-  ) {
+  ) 
+  {
     this.loading = this.loadingCtrl.create();
   }
 
-  ionViewDidLoad() {
-    this.loading.present();
-    this.list2Service
-      .getData()
-      .then(data => {
-        this.list2.items = data.items;
-        this.loading.dismiss();
-      });
-  }
+    ionViewDidLoad() {
+        setTimeout(() => {
+            this.searchbar.setFocus();
+        }, 1000);
+        this.loading.present();
+        this.list2Service
+        .getData()
+        .then(data => {
+            this.list2.items = data.items;
+            this.loading.dismiss();
+        });
+    }
+
+    onCancel()
+    {
+
+    }
 
 }

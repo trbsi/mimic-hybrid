8b234e824c46d7e89e9082b12ea055b12a46804f
Dario Trbovic <timmy.dario@gmail.com>
2017-09-05 08:36:34 +0200

    test video inline

--- src/pages/listing/listing.ts
+++ src/pages/listing/listing.ts
@@ -14,6 +14,7 @@ import { Search } from '../search/search';
 
 import { ViewChild } from '@angular/core';
 import { Slides } from 'ionic-angular';
+import { enableInlineVideo } from 'iphone-inline-video';
 
 @Component({
     selector: 'listing-page',

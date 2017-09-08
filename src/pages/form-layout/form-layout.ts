443f84d15758aac6de4884b553ef697cab3bc3ed
Dario Trbovic <timmy.dario@gmail.com>
2017-09-08 16:18:00 +0200

    work on add mimic page

--- src/pages/form-layout/form-layout.ts
+++ src/pages/form-layout/form-layout.ts
@@ -11,15 +11,9 @@ import { Crop } from '@ionic-native/crop';
 })
 export class FormLayoutPage {
     section:string;
-
     post_form:any;
-    event_form:FormGroup;
-    card_form:FormGroup;
-
-    categories_checkbox_open:boolean;
-    categories_checkbox_result;
-
     selected_image:any;
+    record_upload:any;
 
     constructor(public nav:NavController,
                 public alertCtrl:AlertController,
@@ -27,6 +21,7 @@ export class FormLayoutPage {
                 public imagePicker:ImagePicker) {
 
         this.section = "event";
+        this.record_upload = 'record';
 
         this.post_form = new FormGroup({
             title: new FormControl('', Validators.required),
@@ -35,21 +30,7 @@ export class FormLayoutPage {
             time: new FormControl('01:30', Validators.required),
             temperature: new FormControl(180)
         });
-        this.event_form = new FormGroup({
-            title: new FormControl('', Validators.required),
-            location: new FormControl('', Validators.required),
-            from_date: new FormControl('2016-09-18', Validators.required),
-            from_time: new FormControl('13:00', Validators.required),
-            to_date: new FormControl('', Validators.required),
-            to_time: new FormControl('', Validators.required)
-        });
-        this.card_form = new FormGroup({
-            card_number: new FormControl('', Validators.required),
-            card_holder: new FormControl('', Validators.required),
-            cvc: new FormControl('', Validators.required),
-            exp_date: new FormControl('', Validators.required),
-            save_card: new FormControl(true, Validators.required)
-        });
+
     }
 
     onSegmentChanged(segmentButton:SegmentButton) {
@@ -62,47 +43,6 @@ export class FormLayoutPage {
 
     createPost() {
         console.log(this.post_form.value);
-    }
-
-    createEvent() {
-        console.log(this.event_form.value);
-    }
-
-    createCard() {
-        console.log(this.card_form.value);
-    }
-
-    chooseCategory() {
-        let alert = this.alertCtrl.create({
-            cssClass: 'category-prompt'
-        });
-        alert.setTitle('Category');
-
-        alert.addInput({
-            type: 'checkbox',
-            label: 'Alderaan',
-            value: 'value1',
-            checked: true
-        });
-
-        alert.addInput({
-            type: 'checkbox',
-            label: 'Bespin',
-            value: 'value2'
-        });
-
-        alert.addButton('Cancel');
-        alert.addButton({
-            text: 'Confirm',
-            handler: data => {
-                console.log('Checkbox data:', data);
-                this.categories_checkbox_open = false;
-                this.categories_checkbox_result = data;
-            }
-        });
-        alert.present().then(() => {
-            this.categories_checkbox_open = true;
-        });
     }
 
     openImagePicker() {
